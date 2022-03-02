251d7a5e5f161f4315bf39d0151b6026ef540108
cryptonkid <cryptonkid@gmail.com>
2022-03-02 19:10:19 +0530

    Arrays: Special : Kandens Algorithm: find continous max subarray.

---
+++ src/com/algoexpert/specialAlgos/KandensAlgorithm.java
@@ -0,0 +1,20 @@
+package com.algoexpert.specialAlgos;
+
+public class KandensAlgorithm {
+    /**
+     * Program is to find the maximum of the subarray of the given array.
+     * Complexity : EASY
+     * @param array
+     * @return
+     */
+    public static int kadanesAlgorithm(int[] array) {
+        int maxEndingHere = array[0];
+        int maxSoFar = array[0];
+        for(int i=1;i<array.length;i++){
+            int num = array[i];
+            maxEndingHere = Math.max(num,maxEndingHere + num);
+            maxSoFar = Math.max(maxSoFar,maxEndingHere);
+        }
+        return maxSoFar;
+    }
+}
